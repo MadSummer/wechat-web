2a0c66cc9e65879764676b6facb278694e56debc
liujing <liujing@rongkecloud.com>
2018-03-23 11:18:37 +0800

    统一配置文件

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  DeviceID: 'e706502636959711'//'e' + ('' + Math.random().toFixed(15)).substring(2, 17)
+  DeviceID: 'e706502636959711', //'e' + ('' + Math.random().toFixed(15)).substring(2, 17)
+  BAIDU_APP_ID: '10522749',
+  BAIDU_API_KEY: 'Vqxtkidm7xk1b5bRewCY8ly4',
+  BAIDU_SECRET_KEY: '3bed3a696f6477c33310ffd67d707bd8',
+  TULING_API:'http://www.tuling123.com/openapi/api',
+  TULING_API_KEY:'5a0d9cafeb7243afbc66a17613624c59',
 }
